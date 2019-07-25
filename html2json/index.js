cda19cd2f0d2cb52670d205603af00922094cf4d
Rick Dutour Geerling <info@rickdg.nl>
2019-07-25 20:18:23 +0200

    Shorten presentation by prefilling app

--- html2json/index.js
+++ html2json/index.js
@@ -6,14 +6,6 @@ const fs = require("fs");
 
 class Crawler {
   /**
-   * Construct a crawler from the CLI arguments
-   *
-   * @param {String[]} args List of arguments given on the CLI
-   * @returns {Crawler} Returns new instance of crawler
-   */
-  static fromArgv(args) {}
-
-  /**
    * Creates an instance of Crawler.
    *
    * @param {String|ReadableStream} source The HTML source, can be a filename, URL or a stream to read from
@@ -26,13 +18,43 @@ class Crawler {
   }
 
   /** Fetch the HTML from the source */
-  fetch() {}
+  fetch() {
+    if (typeof this.source === "string" && /^http/.exec(this.source))
+      this.source = got(this.source, { stream: true });
+    else if (typeof this.source === "string")
+      this.source = fs.createReadStream(this.source);
+    return this.getHtmlFromStream();
+  }
+
+  getHtmlFromStream() {
+    return new Promise((resolve, reject) => {
+      let data = "";
+      this.source.on("data", chunk => (data += chunk));
+      this.source.on("error", error => reject(error));
+      this.source.on("end", () => {
+        this.html = data;
+        resolve(data);
+      });
+    });
+  }
 
   /** Parse the HTML and get the selection */
-  parse() {}
+  parse() {
+    const selection = $.load(this.html)(this.selector);
+    this.results = selection
+      .map((i, el) =>
+        $(el)
+          .html()
+          .trim()
+      )
+      .get();
+    return this.results;
+  }
 
   /** Output the results to console in JSON format */
-  output() {}
+  output() {
+    console.log(JSON.stringify(this.results));
+  }
 }
 
 module.exports = Crawler;
--- html2json/spec/html2json.spec.js
+++ html2json/spec/html2json.spec.js
@@ -2,18 +2,6 @@ const Crawler = require("..");
 const fs = require("fs");
 
 describe("html2json", () => {
-  it("should parse CLI arguments", () => {
-    let argv = ["#selector", "filename.html"];
-    let crawler = Crawler.fromArgv(argv);
-    expect(crawler.source).toEqual(argv[1]);
-    expect(crawler.selector).toEqual(argv[0]);
-
-    argv = ["#selector"];
-    crawler = Crawler.fromArgv(argv);
-    expect(crawler.source).toBe(process.stdin);
-    expect(crawler.selector).toEqual(argv[0]);
-  });
-
   it("should parse from file", () => {
     const crawler = new Crawler("spec/test.html", "li");
     return crawler.fetch().then(() => {
