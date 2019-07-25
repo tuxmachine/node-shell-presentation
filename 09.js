6197a2d1b273f7b73f2bdeff5d8b50534446c3ea
Rick Dutour Geerling <info@rickdg.nl>
2019-07-25 21:19:13 +0200

    Add example 9

---
+++ 09.js
@@ -0,0 +1,23 @@
+const fs = require("fs");
+const Crawler = require("./html2json/index");
+
+// Parse HTML from a file
+const file = new Crawler("./spec/test.html", "li");
+file
+  .fetch()
+  .then(() => file.parse())
+  .then(() => file.output());
+
+// Parse HTML from a stream
+const stream = new Crawler(fs.createReadStream("./spec/test.html"), "li");
+stream
+  .fetch()
+  .then(() => stream.parse())
+  .then(() => stream.output());
+
+// Parse HTML from a url
+const url = new Crawler("http://mijnip.nl", "#remoteIPDiv");
+url
+  .fetch()
+  .then(() => url.parse())
+  .then(() => url.output());
