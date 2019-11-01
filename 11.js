4ce9b30034e655a696e0b3ae7a803b583ba22dde
Rick <info@rickdg.nl>
2019-11-01 09:35:20 +0100

    Fix

--- 11.js
+++ 11.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 const childProcess = require('child_process');
-const { readFileSync } = require('fs');
+const fs = require('fs');
 
 // Run external commands in Node.js?
 const cmd = 'head -n 5 jokes.txt';
